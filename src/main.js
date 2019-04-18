c4ddbfa51282bc7c29c12f6e3a537623345b293d
jdinley <jdinley@gmail.com>
2019-04-17 23:39:58 -0400

    Cleaning up the css and adding media queries.

--- src/main.js
+++ src/main.js
@@ -17,20 +17,20 @@ fetch("https://api.github.com/users/jdinley/repos")
 			const projectDateUpdated = `Updated On: ${dateFns.format(new Date(repo.updated_at), "MM/DD/YYYY")}`;
 			const projectDateCreated = `Created On: ${dateFns.format(new Date(repo.created_at), "MM/DD/YYYY")}`;
 			const markup = `
-      <ul>
+      <ul class="projects">
         <li class="project-date">
-          <span class="project-date-updated"> ${projectDateUpdated} </span><br>
-          <span class="project-date-created">${projectDateCreated};</span>
+          <span class="project-date-updated">${projectDateUpdated}</span><br>
+          <span class="project-date-created">${projectDateCreated}</span>
         </li>
         <li class="project-name"><a href="${repo.html_url}">${capWord}</a></li>
 				<li class="project-lang">
-				<span>${`Language: ${repo.language}`};</span><br>
-				<span>${`ID: ${repo.id}`};</span><br>
-				<span>${`Branch: ${repo.branch}`};</span><br>
-				<span>${`License: ${repo.license}`};</span>
+				<span>${`Language: ${repo.language}`}</span><br>
+				<span>${`ID: ${repo.id}`}</span><br>
+				<span>${`Branch: ${repo.branch}`}</span><br>
+				<span>${`License: ${repo.license}`}</span>
 				</li>
         
-        <li class="project-desc">${`Description: ${repo.description}`};</li>
+        <li class="project-desc">${`Description: ${repo.description}`}</li>
       </ul>`;
 
 			document.querySelector(".project").insertAdjacentHTML("beforebegin", markup);
